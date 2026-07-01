9293b05bfaca0861f9ce6f170d6c9bdfd6629764
iki1uc <entertrainer@web.de>
2026-07-01 18:55:07 +0200

    Update FIT.js

--- FIT.js
+++ FIT.js
@@ -1,1 +1,17 @@
+// FIT = Existenz-Impuls
+const FIT = {
+    pos: "itel",
+    calc: "ready",
+    help: "FIT aktiviert andere Module"
+};
+
+// Andere Module FIT machen
+export function makeFIT(target) {
+    target.FIT = {
+        pos: FIT.pos,
+        calc: FIT.calc,
+        help: FIT.help
+    };
+    return target;
+}
 
